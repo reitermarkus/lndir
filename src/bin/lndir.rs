bf0498847fe6501cd6cd7b078a83ad7353bcf708
Markus Reiter <me@reitermark.us>
2022-03-07 07:55:25 +0100

    Fix warnings.

--- src/bin/lndir.rs
+++ src/bin/lndir.rs
@@ -19,45 +19,41 @@ fn parse_args<'a>() -> Result<(Options, Vec<PathBuf>, PathBuf), ArgumentError> {
   let mut iterator = env::args().skip(1);
   let mut stop_option_parsing = false;
 
-  loop {
-    if let Some(arg) = iterator.next() {
-      match arg.as_ref() {
-        "-silent" if !stop_option_parsing => {
-          options.silent = true;
-        },
-        "-ignorelinks" if !stop_option_parsing  => {
-          options.ignore_links = true;
-        },
-        "-withrevinfo" if !stop_option_parsing  => {
-          options.with_rev_info = true;
-        },
-        "-maxdepth" if !stop_option_parsing  => {
-          if let Some(value) = iterator.next() {
-            options.max_depth = match value.parse() {
-              Ok(max_depth) => Some(max_depth),
-              Err(err) => return Err(ArgumentError::new(
-                format!("failed to parse -maxdepth argument \"{}\": {}", value, err),
-                Some(Box::new(err)),
-              )),
-            }
-          } else {
-            return Err(ArgumentError::new(
-              "no value specified for -maxdepth",
-              None,
-            ))
+  while let Some(arg) = iterator.next() {
+    match arg.as_ref() {
+      "-silent" if !stop_option_parsing => {
+        options.silent = true;
+      },
+      "-ignorelinks" if !stop_option_parsing  => {
+        options.ignore_links = true;
+      },
+      "-withrevinfo" if !stop_option_parsing  => {
+        options.with_rev_info = true;
+      },
+      "-maxdepth" if !stop_option_parsing  => {
+        if let Some(value) = iterator.next() {
+          options.max_depth = match value.parse() {
+            Ok(max_depth) => Some(max_depth),
+            Err(err) => return Err(ArgumentError::new(
+              format!("failed to parse -maxdepth argument \"{}\": {}", value, err),
+              Some(Box::new(err)),
+            )),
           }
-        },
-        "--" if !stop_option_parsing => {
-          stop_option_parsing = true;
-        },
-        _ => {
-          stop_option_parsing = true;
+        } else {
+          return Err(ArgumentError::new(
+            "no value specified for -maxdepth",
+            None,
+          ))
+        }
+      },
+      "--" if !stop_option_parsing => {
+        stop_option_parsing = true;
+      },
+      _ => {
+        stop_option_parsing = true;
 
-          paths.push(PathBuf::from(arg));
-        },
-      }
-    } else {
-      break;
+        paths.push(PathBuf::from(arg));
+      },
     }
   }
 
@@ -67,7 +63,7 @@ fn parse_args<'a>() -> Result<(Options, Vec<PathBuf>, PathBuf), ArgumentError> {
     let len = paths.len();
 
     paths.split_off(len - 1).first().map(|d| d.to_owned())
-      .ok_or(ArgumentError::new(
+      .ok_or_else(|| ArgumentError::new(
         "no destination directory specified",
         None,
       ))?
