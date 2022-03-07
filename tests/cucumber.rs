5f6ae992860a712f391dbaf0c76cf1c220b88df8
Markus Reiter <me@reitermark.us>
2022-03-07 08:54:10 +0100

    Switch to `cucumber`.

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,173 +1,155 @@
-#![feature(fnbox)]
+use core::convert::Infallible;
+use std::path::{Path, PathBuf};
+use std::fs::{create_dir_all, read_link, File};
+use std::ffi::OsStr;
+use std::os::unix::fs::symlink;
 
-#[macro_use]
-extern crate cucumber_rust;
-
-extern crate regex;
-extern crate mktemp;
-extern crate lndir;
-
-use std::path::PathBuf;
+use async_trait::async_trait;
+use cucumber::{given, then, when, WorldInit};
+use gherkin::Step;
 use mktemp::Temp;
 
+use lndir::lndir;
 
+#[derive(Debug, WorldInit)]
 pub struct World {
   pub tmpdir: PathBuf,
+  pub paths: Vec<PathBuf>,
 }
 
-impl cucumber_rust::World for World {}
-impl std::default::Default for World {
-  fn default() -> World {
-      World { tmpdir: Temp::new_dir().unwrap().to_path_buf() }
+#[async_trait(?Send)]
+impl cucumber::World for World {
+  type Error = Infallible;
+
+  async fn new() -> Result<Self, Self::Error> {
+    Ok(Self {
+      tmpdir: Temp::new_dir().expect("Failed to create temporary directory").to_path_buf(),
+      paths: vec![],
+    })
   }
 }
 
-mod example_steps {
-  use std::collections::BTreeMap;
-  use std::path::PathBuf;
-  use std::fs::create_dir_all;
-  use std::fs::read_link;
-  use std::fs::File;
-  use std::ffi::OsStr;
-  use lndir::lndir;
-  use std::os::unix::fs::symlink;
+fn parse_directory_tree(string: &String) -> Vec<PathBuf> {
+  let mut lines = string.trim().lines();
 
-  fn parse_directory_tree(string: &String) -> Vec<PathBuf> {
-    let mut lines = string.lines();
+  let mut paths = vec![];
 
-    let mut tree: BTreeMap<PathBuf, Vec<String>> = BTreeMap::new();
+  let mut root = if let Some(line) = lines.next() {
+    PathBuf::from(line)
+  } else {
+    return paths
+  };
 
-    let mut current_key: Option<PathBuf> = None;
+  let mut lines = lines.peekable();
 
-    let root = if let Some(line) = lines.next() {
-      PathBuf::from(line)
+  let mut current_level = 0;
+
+  while let Some(mut line) = lines.next() {
+    let mut level = 0;
+
+    while let Some(s) = line.strip_prefix("│   ") {
+      line = s;
+      level += 1;
+    }
+
+    for _ in 0..(current_level - level) {
+      if let Some(parent) = root.parent() {
+        root = parent.to_owned();
+        current_level -= 1;
+      } else {
+        return paths
+      }
+    }
+
+    if let Some(s) = line.strip_prefix("├── ").or_else(|| line.strip_prefix("└── ")) {
+      root = root.join(s);
+      paths.push(root.clone());
+      current_level += 1;
+    }
+  }
+
+  paths
+}
+
+fn split_path(path: &Path) -> (PathBuf, Option<PathBuf>) {
+  let parts: Vec<PathBuf> = path.to_str().unwrap().split(" → ").map(PathBuf::from).collect();
+
+  let relative_path = match parts[0].strip_prefix(".") {
+    Ok(relative_path) => relative_path.to_owned(),
+    Err(err) => panic!("Failed to strip prefix from path {}: {}", path.to_str().unwrap(), err),
+  };
+  let relative_symlink_path = parts.get(1).map(PathBuf::to_owned);
+
+  (relative_path, relative_symlink_path)
+}
+
+#[given(expr = "the directory structure")]
+async fn directory_structure(world: &mut World, step: &Step) {
+  let docstring = step.to_owned().docstring.unwrap();
+
+  world.paths = parse_directory_tree(&docstring);
+
+  for path in &world.paths {
+    let (relative_path, relative_symlink_path) = split_path(&path);
+
+    let absolute_path = world.tmpdir.join(&relative_path);
+
+    let file_name = absolute_path.file_name().and_then(OsStr::to_str).unwrap();
+
+    if let Some(relative_symlink_path) = relative_symlink_path {
+      create_dir_all(&absolute_path.parent().unwrap()).unwrap();
+      symlink(&relative_symlink_path, &absolute_path).unwrap();
+    } else if file_name.ends_with(".d") {
+      create_dir_all(&absolute_path).unwrap();
     } else {
-      return Vec::new()
-    };
-
-    loop {
-      if let Some(line) = lines.next() {
-        let prefix: String = line.chars().take(4).collect();
-        let mut string: String = line.chars().skip(4).collect();
-
-        if prefix == "├── " || prefix == "└── " {
-          let path = PathBuf::from(string.to_owned());
-          current_key = Some(path.to_owned());
-          tree.insert(path, vec![string]);
-        } else if let Some(key) = &current_key {
-          if let Some(lines) = tree.get_mut(key) {
-            lines.push(string)
-          }
-        }
-      } else {
-        break;
-      }
+      create_dir_all(&absolute_path.parent().unwrap()).unwrap();
+      File::create(&absolute_path).unwrap();
     }
-
-    let mut paths: Vec<PathBuf> = Vec::new();
-
-    for (path, lines) in tree {
-      if lines.len() > 1 {
-        let doc = lines.join("\n");
-        let mut sub_paths = parse_directory_tree(&doc).iter().map(|sub_path| root.join(sub_path) ).collect();
-
-        paths.append(&mut sub_paths);
-      } else {
-        paths.push(root.join(path.to_owned()));
-      }
-    }
-
-    paths
-  }
-
-  fn split_path(path: PathBuf) -> (PathBuf, Option<PathBuf>) {
-    let parts: Vec<PathBuf> = path.to_str().unwrap().split(" → ").map(PathBuf::from).collect();
-
-    let relative_path = parts[0].strip_prefix(".").unwrap().to_owned();
-    let relative_symlink_path = parts.get(1).map(PathBuf::to_owned);
-
-    (relative_path, relative_symlink_path)
-  }
-
-  steps! {
-    world: ::World; // Any type that implements Default can be the world
-
-    given "the directory structure" |world, step| {
-      let docstring = step.to_owned().docstring.unwrap();
-
-      let paths = parse_directory_tree(&docstring);
-
-      for path in paths {
-        let (relative_path, relative_symlink_path) = split_path(path);
-
-        let absolute_path = world.tmpdir.join(&relative_path);
-
-        let file_name = absolute_path.file_name().and_then(OsStr::to_str).unwrap();
-
-        if let Some(relative_symlink_path) = relative_symlink_path {
-          create_dir_all(&absolute_path.parent().unwrap()).unwrap();
-          symlink(&relative_symlink_path, &absolute_path).unwrap();
-        } else if file_name.ends_with(".d") {
-          create_dir_all(&absolute_path).unwrap();
-        } else {
-          create_dir_all(&absolute_path.parent().unwrap()).unwrap();
-          File::create(&absolute_path).unwrap();
-        }
-      }
-    };
-
-    when regex r"I run `lndir\s+([^`]*)`" |world, matches, _step| {
-      let mut arguments: Vec<PathBuf> = matches[1].split(" ").map(|arg| world.tmpdir.join(PathBuf::from(arg))).collect();
-
-      let len = arguments.len();
-
-      let destination = arguments.split_off(len - 1).first().unwrap().to_owned();
-      let sources = arguments;
-
-      lndir(sources, destination, None).unwrap();
-    };
-
-    then "the resulting directory structure is" |world, step| {
-      let docstring = step.to_owned().docstring.unwrap();
-
-      let paths = parse_directory_tree(&docstring);
-
-      for path in paths {
-       let (relative_path, relative_symlink_path) = split_path(path);
-
-       let absolute_path = world.tmpdir.join(relative_path.to_owned());
-       let dir = absolute_path.parent().unwrap();
-       let absolute_symlink_path = relative_symlink_path.to_owned().map(|path| dir.join(path));
-
-       let file_name = absolute_path.file_name().and_then(OsStr::to_str).unwrap();
-
-       if let Some(relative_symlink_path) = relative_symlink_path {
-         if let Ok(symlink_path) = read_link(&absolute_path) {
-           if relative_symlink_path.starts_with("..") {
-             assert_eq!(dir.join(symlink_path).canonicalize().unwrap(), absolute_symlink_path.unwrap().canonicalize().unwrap());
-           } else {
-             assert_eq!(symlink_path, relative_symlink_path);
-           }
-         } else {
-           panic!("{} is not a symlink.", relative_path.to_string_lossy());
-         }
-       } else if file_name.ends_with(".d") {
-         assert!(absolute_path.is_dir(), format!("{} is not a directory.", relative_path.to_string_lossy()));
-       } else {
-         assert!(absolute_path.is_file(), format!("{} is not a file.", relative_path.to_string_lossy()));
-       }
-      }
-    };
   }
 }
 
-cucumber! {
-  features: "./features"; // Path to our feature files
-  world: ::World; // The world needs to be the same for steps and the main cucumber call
-  steps: &[
-    example_steps::steps // the `steps!` macro creates a `steps` function in a module
-  ];
-  before: || {
-    // Called once before everything; optional.
+#[when(regex = r"I run `lndir\s+([^`]*)`")]
+async fn run_lndir(world: &mut World, matches: &[String]) {
+  let mut arguments: Vec<PathBuf> = matches[0].split(" ").map(|arg| world.tmpdir.join(PathBuf::from(arg))).collect();
+
+  let len = arguments.len();
+
+  let destination = arguments.split_off(len - 1).first().unwrap().to_owned();
+  let sources = arguments;
+
+  lndir(sources, destination, None).unwrap();
+}
+
+#[then(expr = "the resulting directory structure is")]
+async fn resulting_directory_structure(world: &mut World) {
+  for path in &world.paths {
+   let (relative_path, relative_symlink_path) = split_path(&path);
+
+   let absolute_path = world.tmpdir.join(relative_path.to_owned());
+   let dir = absolute_path.parent().unwrap();
+   let absolute_symlink_path = relative_symlink_path.to_owned().map(|path| dir.join(path));
+
+ let file_name = absolute_path.file_name().and_then(OsStr::to_str).unwrap();
+
+   if let Some(relative_symlink_path) = relative_symlink_path {
+     if let Ok(symlink_path) = read_link(&absolute_path) {
+       if relative_symlink_path.starts_with("..") {
+         assert_eq!(dir.join(symlink_path).canonicalize().unwrap(), absolute_symlink_path.unwrap().canonicalize().unwrap());
+       } else {
+         assert_eq!(symlink_path, relative_symlink_path);
+       }
+     } else {
+       panic!("{} is not a symlink.", relative_path.to_string_lossy());
+     }
+   } else if file_name.ends_with(".d") {
+     assert!(absolute_path.is_dir(), "{} is not a directory.", relative_path.to_string_lossy());
+   } else {
+     assert!(absolute_path.is_file(), "{} is not a file.", relative_path.to_string_lossy());
+   }
   }
+}
+
+#[tokio::main]
+async fn main() {
+  World::run("features").await
 }
